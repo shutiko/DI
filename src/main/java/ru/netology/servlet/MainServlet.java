9b47cf4d0c7d8606d771541c9aa5039bfe141db3
Borblik Oleg <likorff@list.ru>
2023-06-21 16:58:31 +0700

    first commit

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,64 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  public static final String REQUEST_GET = "GET";
+  public static final String REQUEST_POST = "POST";
+  public static final String REQUEST_DELETE = "DELETE";
+
+
+
+  @Override
+  public void init() {
+    final var repository = new PostRepository();
+    final var service = new PostService(repository);
+    controller = new PostController(service);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    // если деплоились в root context, то достаточно этого
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+      // primitive routing
+      if (method.equals(REQUEST_GET) && path.equals("/api/posts")) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals(REQUEST_GET) && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals(REQUEST_POST) && path.equals("/api/posts")) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals(REQUEST_DELETE) && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
+
